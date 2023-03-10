8742c085a2a449e8d2416a23bd7b0b276a14b5c1
Arnold Szabo <szaboaarnold@gmail.com>
2023-03-10 12:44:48 +0100

    Add sample d2g sample project

--- d2g/src/main/java/com/dice/doris/example/PlayerActivity.java
+++ d2g/src/main/java/com/dice/doris/example/PlayerActivity.java
@@ -3,15 +3,19 @@ package com.dice.doris.example;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.dice.shield.drm.entity.ActionToken;
+import com.dice.shield.drm.utils.Utils;
 import com.diceplatform.doris.ExoDoris;
 import com.diceplatform.doris.ExoDorisBuilder;
 import com.diceplatform.doris.entity.Source;
 import com.diceplatform.doris.entity.SourceBuilder;
 import com.diceplatform.doris.ui.ExoDorisPlayerView;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.util.EventLogger;
 
-public class PlayerActivity extends BasePlayerActivity {
+public class PlayerActivity extends AppCompatActivity {
 
     private ExoDoris player;
 
@@ -20,18 +24,27 @@ public class PlayerActivity extends BasePlayerActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
+        Bundle extras = getIntent().getExtras();
+
+        VideoItem videoItem = (VideoItem) extras.getSerializable("video");
+
         player = new ExoDorisBuilder(this)
                 .setPlayWhenReady(true)
                 .build();
+        player.addAnalyticsListener(new EventLogger());
 
         ExoDorisPlayerView playerView = findViewById(R.id.playerView);
         playerView.setPlayer(player.getExoPlayer());
 
         MediaItem.Builder mediaItemBuilder = new MediaItem.Builder()
-                .setUri(getString(R.string.dash_video_url));
+                .setUri(videoItem.getUrl());
 
         Source source = new SourceBuilder()
                 .setMediaItem(mediaItemBuilder.build())
+                .setId(videoItem.getId())
+                .setShouldPlayOffline(true)
+                .setDrmParams(new ActionToken(Utils.DRM_SCHEME, videoItem.getOfflineLicense()))
+                .setOfflineLicense(videoItem.getOfflineLicense())
                 .build();
 
         player.load(source);
